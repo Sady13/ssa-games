4f42a3fae01fed5e59b87528499fd7bcd6fce483
Sady13 <42273751+Sady13@users.noreply.github.com>
2018-08-30 16:33:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,12 @@
 const Discord = require("discord.js");
 const  client = new Discord.Client();
-const config = require("./config.json");
 
 client.on("ready", () => {
    console.log("Estoy listo!");
 });
-var prefix = config.prefix;
-
 client.on("message", (message) => {
-      if (message.content.startsWith(prefix +"ip")){
-    message.channel.send({embed: {
-      color: 3447003,
-      description: "el servidor esta cerrado."
-    }});
-  } else
-  if (message.content.startsWith(prefix +"port")){
-    message.channel.send({embed: {
-      color: 3447003,
-      description: "el servidor esta cerrado."
-    }});
-}
+  if (message.content.startsWith("!ip")) {
+    message.channel.send("servidor cerrado.");
+  }
 });
-client.login(config.token);
-
-client.on("guildMemberAdd", (member) => {
-   console.log(`Nuevo usuario:  ${member.user.username} se ha unido a ${member.guild.name}.`);
-   var canal = client.channels.get('473873051868463106');
-   canal.send(`${member.user}, bienvenido al servidor pasalo bien.`);
-
-});
+client.login("TokenSecreto");       
